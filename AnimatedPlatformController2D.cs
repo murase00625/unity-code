59d6d2251f4667672841958e614caf71baa72303
Morio Murase <melchior00@gmail.com>
2015-07-29 14:13:48 -0700

    convert tabs to spaces

--- AnimatedPlatformController2D.cs
+++ AnimatedPlatformController2D.cs
@@ -2,12 +2,12 @@ using UnityEngine;
 using System.Collections;
 
 public class AnimatedPlatformController2D : MonoBehaviour {
-	private bool facingRight = true;
-	private bool isJumping = false;
-	public bool gamestarted = false;
+    private bool facingRight = true;
+    private bool isJumping = false;
+    public bool gamestarted = false;
 
     // Flip this to true when the animation state machine is ready
-	private bool animated = false;
+    private bool animated = false;
 
     private Transform groundCheckLeft, groundCheckRight;
     public bool grounded = false;
@@ -22,93 +22,93 @@ public class AnimatedPlatformController2D : MonoBehaviour {
     public ParticleSystem ouch;
 
     void Awake() {
-    	gamestarted = true;
-    	groundCheckLeft = transform.Find("groundCheckLeft");
-    	groundCheckRight = transform.Find("groundCheckRight");
-    	if (animated) anim = GetComponent<Animator>();
-    	if (ouch != null) ouch.Stop();
+        gamestarted = true;
+        groundCheckLeft = transform.Find("groundCheckLeft");
+        groundCheckRight = transform.Find("groundCheckRight");
+        if (animated) anim = GetComponent<Animator>();
+        if (ouch != null) ouch.Stop();
     }
 
-	// Jumping requires all platforms and "jumpable" objects to be in a layer called Jumpable,
-	// and two empty objects at the character's "feet" called "groundCheckLeft" and "groundCheckRight".
-	// Jumpable must be an object layer in Unity, NOT a sprite sorting layer!
-	void Update() {
-		currentRunSpeed = Mathf.Abs(rigidbody2D.velocity.x);
-		if (gamestarted) {
-			float h = Input.GetAxis("Horizontal");
+    // Jumping requires all platforms and "jumpable" objects to be in a layer called Jumpable,
+    // and two empty objects at the character's "feet" called "groundCheckLeft" and "groundCheckRight".
+    // Jumpable must be an object layer in Unity, NOT a sprite sorting layer!
+    void Update() {
+        currentRunSpeed = Mathf.Abs(rigidbody2D.velocity.x);
+        if (gamestarted) {
+            float h = Input.GetAxis("Horizontal");
 
-			if ((h > 0 && !facingRight) || (h < 0 && facingRight)) {
-				Flip();
-			}
-			RaycastHit2D hitLeft = Physics2D.Raycast(groundCheckLeft.position, -Vector2.up, 0.1f, 1 << LayerMask.NameToLayer("Jumpable"));
-			RaycastHit2D hitRight = Physics2D.Raycast(groundCheckRight.position, -Vector2.up, 0.1f, 1 << LayerMask.NameToLayer("Jumpable"));
-			grounded = hitLeft.collider != null || hitRight.collider != null;
+            if ((h > 0 && !facingRight) || (h < 0 && facingRight)) {
+                Flip();
+            }
+            RaycastHit2D hitLeft = Physics2D.Raycast(groundCheckLeft.position, -Vector2.up, 0.1f, 1 << LayerMask.NameToLayer("Jumpable"));
+            RaycastHit2D hitRight = Physics2D.Raycast(groundCheckRight.position, -Vector2.up, 0.1f, 1 << LayerMask.NameToLayer("Jumpable"));
+            grounded = hitLeft.collider != null || hitRight.collider != null;
 
-			if (Input.GetButtonDown("Jump") && grounded) {
-				isJumping = true;
-			}
-		}
-	}
+            if (Input.GetButtonDown("Jump") && grounded) {
+                isJumping = true;
+            }
+        }
+    }
 
-	void FixedUpdate() {
-		if (gamestarted) {
-			float h = Input.GetAxis("Horizontal");
+    void FixedUpdate() {
+        if (gamestarted) {
+            float h = Input.GetAxis("Horizontal");
 
-			if (animated) anim.SetFloat("Speed", Mathf.Abs(h));
-			float speed = Mathf.Abs(rigidbody2D.velocity.x);
+            if (animated) anim.SetFloat("Speed", Mathf.Abs(h));
+            float speed = Mathf.Abs(rigidbody2D.velocity.x);
 
-			if (speed < maxRunSpeed) {
-				float asymptoticForce = runForce * (1 - speed/maxRunSpeed);
-				if (changeDirectionsMidJump || grounded)
-				rigidbody2D.AddForce(Vector2.right * h * asymptoticForce);
-				} else {
+            if (speed < maxRunSpeed) {
+                float asymptoticForce = runForce * (1 - speed/maxRunSpeed);
+                if (changeDirectionsMidJump || grounded)
+                rigidbody2D.AddForce(Vector2.right * h * asymptoticForce);
+                } else {
 
-				}
+                }
 
-				if (isJumping) {
-					if (animated) anim.SetTrigger("Jump");
+                if (isJumping) {
+                    if (animated) anim.SetTrigger("Jump");
 
-					rigidbody2D.AddForce(new Vector2(0f, jumpForce));
-					isJumping = false;
-				}
+                    rigidbody2D.AddForce(new Vector2(0f, jumpForce));
+                    isJumping = false;
+                }
 
-				if (animated) {
-					AnimatorStateInfo stateInfo = anim.GetCurrentAnimatorStateInfo(0);
+                if (animated) {
+                    AnimatorStateInfo stateInfo = anim.GetCurrentAnimatorStateInfo(0);
 
-					if (stateInfo.IsName("Base Layer.jump")) {
-						if (grounded) {
-							anim.SetTrigger("Touchground");
-						}
-					}
-				}
-			}
-		}
+                    if (stateInfo.IsName("Base Layer.jump")) {
+                        if (grounded) {
+                            anim.SetTrigger("Touchground");
+                        }
+                    }
+                }
+            }
+        }
 
-		void Flip() {
-			facingRight = !facingRight;
+        void Flip() {
+            facingRight = !facingRight;
 
-			Vector3 theScale = transform.localScale;
-			theScale.x *= -1;
-			transform.localScale = theScale;
-		}
+            Vector3 theScale = transform.localScale;
+            theScale.x *= -1;
+            transform.localScale = theScale;
+        }
 
-		void StopPlayer() {
-			gamestarted = false;
-			print("Ouch! The player has faceplanted.");
+        void StopPlayer() {
+            gamestarted = false;
+            print("Ouch! The player has faceplanted.");
 
-			if (transform.localScale.x < 0) Flip();
-			if (ouch != null) ouch.Play();
-			StartCoroutine("waitForRestart");
-		}
+            if (transform.localScale.x < 0) Flip();
+            if (ouch != null) ouch.Play();
+            StartCoroutine("waitForRestart");
+        }
 
-		void EndPlay() {
-			gamestarted = false;
-			StartCoroutine("waitForRestart");      
-		}
+        void EndPlay() {
+            gamestarted = false;
+            StartCoroutine("waitForRestart");      
+        }
 
-		IEnumerator waitForRestart() {
-			yield return new WaitForSeconds(timeToRestart);
-			Application.LoadLevel(0);
-		}
+        IEnumerator waitForRestart() {
+            yield return new WaitForSeconds(timeToRestart);
+            Application.LoadLevel(0);
+        }
 
-	}
+    }

e7056ef524f23a1e73a809b237eadb8bebed447e
Morio Murase <melchior00@gmail.com>
2015-07-29 14:44:46 -0700

    indentation fixes, merged raycasting method

--- PlatformCameraFollow.cs
+++ PlatformCameraFollow.cs
@@ -8,50 +8,50 @@ using System.Collections;
 // The target is the player character object, and the background is the in-scene sprite
 //  object that bounds the camera's viewport (i.e. the screen).
 
-// Known issue: If the screen starts at the left edge or right edge of the background,
-//  and the camera is the view might glitch for a split second as the player first moves toward the middle.
+// Known issue: If the camera view starts at the left edge or right edge of the background,
+//  the view might glitch for a split second as the player first moves toward the middle.
 //  Possibly a Unity camera bug in 4.7f that is fixed in a later version.
 // Workaround: Position the camera right on top of the player object.
 public class PlatformCameraFollow : MonoBehaviour {
- 
-        public Transform target;
-        public SpriteRenderer background;
-        
-        // The bounding box for the camera
-        private float maxX, maxY, minX, minY;
-        private float targetX, targetY;
-       
-        void Start () {
-                Bounds bounds = background.renderer.bounds;
-                float cameraHalfHeight = camera.orthographicSize;
-                float cameraHalfWidth = cameraHalfHeight * Screen.width / Screen.height;
- 
-                minX = bounds.min.x + cameraHalfWidth;
-                minY = bounds.min.y + cameraHalfHeight;
-                maxX = bounds.max.x - cameraHalfWidth;
-                maxY = bounds.max.y - cameraHalfHeight;
+
+    public Transform target;
+    public SpriteRenderer background;
+
+    // The bounding box for the camera
+    private float maxX, maxY, minX, minY;
+    private float targetX, targetY;
+
+    void Start () {
+        Bounds bounds = background.renderer.bounds;
+        float cameraHalfHeight = camera.orthographicSize;
+        float cameraHalfWidth = cameraHalfHeight * Screen.width / Screen.height;
+
+        minX = bounds.min.x + cameraHalfWidth;
+        minY = bounds.min.y + cameraHalfHeight;
+        maxX = bounds.max.x - cameraHalfWidth;
+        maxY = bounds.max.y - cameraHalfHeight;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        targetX = target.transform.position.x;
+        targetY = target.transform.position.y;
+    }
+
+    void LateUpdate() {
+        Vector3 curPosition = transform.position;
+
+        if (targetX > minX && targetX < maxX) {
+            curPosition.x = targetX;
+        } else {
+                curPosition.x = Mathf.Clamp(curPosition.x, minX, maxX);
         }
- 
-        // Update is called once per frame
-        void Update () {
-                targetX = target.transform.position.x;
-                targetY = target.transform.position.y;
+        if (targetY > minY && targetY < maxY) {
+            curPosition.y = targetY;
+        } else {
+            curPosition.y = Mathf.Clamp(curPosition.y, minY, maxY);
         }
- 
-        void LateUpdate() {
-                Vector3 curPosition = transform.position;
- 
-                if (targetX > minX && targetX < maxX) {
-                        curPosition.x = targetX;
-                } else {
-                        curPosition.x = Mathf.Clamp(curPosition.x, minX, maxX);
-                }
-                if (targetY > minY && targetY < maxY) {
-                        curPosition.y = targetY;
-                } else {
-                        curPosition.y = Mathf.Clamp(curPosition.y, minY, maxY);
-                }
- 
-                transform.position = curPosition;
-        }
+
+        transform.position = curPosition;
+    }
 }
--- PlatformController2D.cs
+++ PlatformController2D.cs
@@ -1,104 +1,104 @@
 using UnityEngine;
 using System.Collections;
- 
-public class CharacterController2D : MonoBehaviour {
- 
-        private bool facingRight = true;
-        private bool jump = false;
-        private bool grounded = false;
-        private bool gamestarted = false;
- 
-        public float currentRunSpeed = 0f;
-        public float jumpForce = 500f;
-        public float runForce = 350f;
-        public float maxJump = 2f;
-        public float maxRunSpeed = 5f;
-        public ParticleSystem ouch;
-        // Super Mario style jumping: Toggle this to true to allow redirection mid-jump.
-        public bool changeDirectionsMidJump = false;
- 
-        // Use this for initialization
-        void Start () {
-                gamestarted = true;
-                // ouch.Stop();
-        }
-       
-        // Update is called once per frame, and is appropriate for things that don't require a ton of precision.
-        void Update () {
-                if (gamestarted) {
-                        float h = Input.GetAxis("Horizontal");
- 
-                        if ((h > 0 && !facingRight) || (h < 0 && facingRight)) {
-                                Flip();
-                        }
- 
-                        if (Input.GetKeyDown("space") && jump == false) {
-                                if (grounded == true) {
-                                        jump = true;
-                                        grounded = false;
-                                }
-                        }
- 
-                }
-        }
-        
 
-        // FixedUpdate is called much more quickly than Update, and is appropriate for realtime physics calculations.
-        void FixedUpdate() {
-                if (jump == true) {
-                        if (Mathf.Abs(rigidbody2D.velocity.y) < maxJump)
-                                rigidbody2D.AddForce(new Vector2(0f, jumpForce));
- 
-                        if (Mathf.Abs(rigidbody2D.velocity.y) >= maxJump)
-                                rigidbody2D.velocity = new Vector2(rigidbody2D.velocity.x, maxJump);
-                        jump = false;
+public class CharacterController2D : MonoBehaviour {
+
+    private bool facingRight = true;
+    private bool jump = false;
+    private bool grounded = false;
+    private bool gamestarted = false;
+
+    public float timeToRestart = 3f;
+    public float currentRunSpeed = 0f;
+    public float jumpForce = 500f;
+    public float runForce = 350f;
+    public float maxJump = 2f;
+    public float maxRunSpeed = 5f;
+    public ParticleSystem ouch;
+    // Super Mario style jumping: Toggle this to true to allow redirection mid-jump.
+    public bool changeDirectionsMidJump = false;
+
+    // Use this for initialization
+    void Start () {
+        gamestarted = true;
+        groundCheckLeft = transform.Find("groundCheckLeft");
+        groundCheckRight = transform.Find("groundCheckRight");
+        if (ouch != null) ouch.Stop();
+    }
+
+    // Update is called once per frame, and is appropriate for things that don't require a ton of precision.
+    void Update () {
+        if (gamestarted) {
+            float h = Input.GetAxis("Horizontal");
+
+            if ((h > 0 && !facingRight) || (h < 0 && facingRight)) {
+                Flip();
+            }
+
+            RaycastHit2D hitLeft = Physics2D.Raycast(groundCheckLeft.position, -Vector2.up, 0.1f, 1 << LayerMask.NameToLayer("Jumpable"));
+            RaycastHit2D hitRight = Physics2D.Raycast(groundCheckRight.position, -Vector2.up, 0.1f, 1 << LayerMask.NameToLayer("Jumpable"));
+            grounded = hitLeft.collider != null || hitRight.collider != null;
+
+            if (Input.GetKeyDown("space") && jump == false) {
+                if (grounded == true) {
+                    jump = true;
                 }
- 
-                float h = Input.GetAxis("Horizontal");
- 
-                if (h * rigidbody2D.velocity.x < maxRunSpeed) {
-                        if (changeDirectionsMidJump || grounded) {
-                                rigidbody2D.AddForce(Vector2.right * h * runForce);
-                        }
-                }
- 
-                if (Mathf.Abs(rigidbody2D.velocity.x) > maxRunSpeed) {
-                        rigidbody2D.velocity = new Vector2(Mathf.Sign(rigidbody2D.velocity.x) * maxRunSpeed, rigidbody2D.velocity.y);
-                }
-                currentRunSpeed = rigidbody2D.velocity.x;
- 
+            }
+
         }
- 
-        void Flip() {
-                facingRight = !facingRight;
- 
-                Vector3 theScale = transform.localScale;
-                theScale.x *= -1;
-                transform.localScale = theScale;
+    }
+
+
+    // FixedUpdate is called much more quickly than Update, and is appropriate for realtime physics calculations.
+    void FixedUpdate() {
+        if (jump == true) {
+            if (Mathf.Abs(rigidbody2D.velocity.y) < maxJump)
+            rigidbody2D.AddForce(new Vector2(0f, jumpForce));
+
+            if (Mathf.Abs(rigidbody2D.velocity.y) >= maxJump)
+            rigidbody2D.velocity = new Vector2(rigidbody2D.velocity.x, maxJump);
+            jump = false;
         }
- 
-        void StopPlayer() {
-                gamestarted = false;
-                print("Ouch! The player has faceplanted.");
- 
-                if (transform.localScale.x < 0) Flip();
- 
-                // ouch.Play();
-                StartCoroutine("waitForRestart");
+
+        float h = Input.GetAxis("Horizontal");
+
+        if (h * rigidbody2D.velocity.x < maxRunSpeed) {
+            if (changeDirectionsMidJump || grounded) {
+                rigidbody2D.AddForce(Vector2.right * h * runForce);
+            }
         }
- 
-        void EndPlay() {
-                gamestarted = false;
-                StartCoroutine("waitForRestart");      
+
+        if (Mathf.Abs(rigidbody2D.velocity.x) > maxRunSpeed) {
+            rigidbody2D.velocity = new Vector2(Mathf.Sign(rigidbody2D.velocity.x) * maxRunSpeed, rigidbody2D.velocity.y);
         }
- 
-        IEnumerator waitForRestart() {
-                yield return new WaitForSeconds(5);
-                Application.LoadLevel(0);
-        }
- 
-        void OnCollisionEnter2D (Collision2D hit) {
-                grounded = true;
-        }
- 
+        currentRunSpeed = rigidbody2D.velocity.x;
+
+    }
+
+    void Flip() {
+        facingRight = !facingRight;
+
+        transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
+    }
+
+    void StopPlayer() {
+        gamestarted = false;
+        print("Ouch! The player has faceplanted.");
+
+        if (transform.localScale.x < 0) Flip();
+
+        if (ouch != null) ouch.Play();
+        StartCoroutine("waitForRestart");
+    }
+
+    void EndPlay() {
+        gamestarted = false;
+        StartCoroutine("waitForRestart");      
+    }
+
+    IEnumerator waitForRestart() {
+        yield return new WaitForSeconds(timeToRestart);
+        Application.LoadLevel(0);
+    }
+
 }

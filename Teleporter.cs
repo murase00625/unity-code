efbd6f589077ba13a20f509cc27cad1421bbdb10
Morio Murase <melchior00@gmail.com>
2015-10-27 10:39:55 -0700

    Teleportation script

---
+++ Teleporter.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Teleporter : MonoBehaviour {
+
+	public Transform destination;
+
+	void OnTriggerEnter2D(Collider2D collider) {
+		if (collider.gameObject.tag == "Player" && destination != null) {
+			collider.gameObject.transform.position = destination.position;
+		}
+	}
+
+}
